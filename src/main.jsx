453daeae0273f433cf9cc6179bb5268439b2ff7d
Ali Raza Khan <ali.khan@septemsystems.com>
2023-04-02 17:42:44 +0500

    test commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./index.css";
 import "antd/dist/reset.css";
 import { antdTheme } from "./constants/antdTheme";
 
+// inject app into DOM
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ConfigProvider theme={antdTheme}>
